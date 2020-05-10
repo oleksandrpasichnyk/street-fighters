38f55c1d9e01adb3174226f39ef09e19d5bf2405
Oleksandr Pasichnyk <oleksandr.pasichnyk.work@gmail.com>
2020-05-10 23:31:49 +0300

    Initial commit

---
+++ src/javascript/components/fighterPreview.js
@@ -0,0 +1,39 @@
+import { createElement } from '../helpers/domHelper';
+
+export function createFighterPreview(fighter, position) {
+  let {source, name, health, attack, defense} = fighter;
+  const positionClassName = position === 'right' ? 'fighter-preview___right fighter-preview' : 'fighter-preview___left fighter-preview';
+  const fighterElement = createElement({
+    tagName: 'div',
+    className: `fighter-preview___root ${positionClassName}`,
+  });
+
+  console.log(fighter);
+  let preview = document.createElement("div");
+  preview.innerHTML = `<img src="${source}" alt="">
+                <h1>${name}</h1>
+                <p>Health: ${health} </p>
+                <p>Attack: ${attack}</p>
+                <p>Defence: ${defense}</p>`
+  fighterElement.append(preview);
+
+  // todo: show fighter info (image, name, health, etc.)
+
+  return fighterElement;
+}
+
+export function createFighterImage(fighter) {
+  const { source, name } = fighter;
+  const attributes = { 
+    src: source, 
+    title: name,
+    alt: name 
+  };
+  const imgElement = createElement({
+    tagName: 'img',
+    className: 'fighter-preview___img',
+    attributes,
+  });
+
+  return imgElement;
+}
---
+++ src/javascript/services/fightersService.js
@@ -0,0 +1,27 @@
+import { callApi } from '../helpers/apiHelper';
+
+class FighterService {
+  async getFighters() {
+    try {
+      const endpoint = 'fighters.json';
+      const apiResult = await callApi(endpoint, 'GET');
+      return apiResult;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async getFighterDetails(id) {
+    // todo: implement this method
+    try {
+      const endpoint = `details/fighter/${id}.json`
+      const apiResult = await callApi(endpoint, 'GET');
+      return apiResult;
+    } catch (error) {
+      throw error;
+    }
+    // endpoint - `details/fighter/${id}.json`;
+  }
+}
+
+export const fighterService = new FighterService();
